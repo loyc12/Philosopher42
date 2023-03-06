6590c8fd249452167681a63e13be1615faacfd17
Loic Lord <llord@c2r1p1.42quebec.com>
2023-03-06 14:24:19 -0500

    planing things

--- include/philosopher.h
+++ include/philosopher.h
@@ -6,7 +6,7 @@
 /*   By: llord <llord@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 12:56:01 by llord             #+#    #+#             */
-/*   Updated: 2023/03/02 09:25:04 by llord            ###   ########.fr       */
+/*   Updated: 2023/03/06 14:23:24 by llord            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,23 @@
 // ======== LIBRARIES ======== //
 
 # include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <semaphore.h>
 
-// ======== ERROR MESSAGES ======== //
+// ======== DEFINITIONS ======== //
 
-# define ERR_PIPE	"Process Error : Couldn't pipe() properly\n"
-# define ERR_FORK	"Process Error : Couldn't fork() properly\n"
-# define ERR_EXIT	"Process Warning : Child did not exit properly\n"
+# define ERR_ARG	"Input Error : Invalid argument count"
+# define ERR_NUM	"Input Error : Non-digit input are invalid"
+# define ERR_INIT	"Process Error : Initialization failure"
+# define ERR_MUT	"Process Error : Mutex failure"
 
-# define ERR_ARG	"Input Error : Invalid argument given\n"
-# define ERR_ARG_NB	"Input Error : Invalid argument number\n"
-# define ERR_QUOTES	"Input Error : Invalid quotation (unclosed)\n"
+# define ACT_TAKE	"has taken a fork";
+# define ACT_EAT	"is eating";
+# define ACT_SLEEP	"is sleeping";
+# define ACT_THINK	"is thinking";
+# define ACT_DIE	"died";
 
 # define ADRS		(void **)&
 
@@ -34,18 +41,61 @@
 enum e_mstate
 {
 	MSTATE_ERROR	= -1,
-	MSTATE_RUNING	= 0,
-	MSTATE_STARTING	= 1,
+	MSTATE_STARTING	= 0,
+	MSTATE_RUNING	= 1,
 	MSTATE_ENDING	= 2
+};
+
+enum e_pstate
+{
+	PSTATE_DEAD		= -1,
+	PSTATE_SLEEPING	= 0,
+	PSTATE_THINKING	= 1,
+	PSTATE_EATING	= 2,
 };
 
 // ======== STRUCTS ======== //
 
+typedef struct s_meta	t_meta;
+
+typedef struct s_fork
+{
+	pthread_mutex_t	f_mutex;
+	int				fork_id;
+	int				user_id;
+}					t_fork;
+
+typedef struct s_philo
+{
+	t_meta	*m;
+	t_fork	*right_fork;
+	t_fork	*left_fork;
+
+	int		philo_id;
+	int		meal_count;
+
+}			t_philo;
+
 typedef struct s_meta
 {
-	int		state;
-}			t_meta;
+	int			philo_count;
+	int			time_death;		//in ms
+	int			time_eat;		//in ms
+	int			time_sleep;		//in ms
+	int			meal_limit;		//optional argument, ends program when reached by all philosophers
+
+	pthread_t	*philo_threads;
+
+	t_fork		**forks;
+	t_philo		**philos;
+
+	int			start_time;
+	int			state;
+}				t_meta;
 
 // ======== FUNCTIONS ======== //
+
+//from freeer
+void	free_null(void **ptr);
 
 #endif
--- src/freeer.c
+++ src/freeer.c
@@ -6,8 +6,15 @@
 /*   By: llord <llord@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 13:00:02 by llord             #+#    #+#             */
-/*   Updated: 2023/03/01 14:46:34 by llord            ###   ########.fr       */
+/*   Updated: 2023/03/06 13:54:33 by llord            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosopher.h"
+
+//takes a pointer's adress and frees whatever is there, setting it to NULL after
+void	free_null(void **ptr)
+{
+	free(*ptr);
+	*ptr = NULL;
+}
